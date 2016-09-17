b8a8beb4dcb647babcea764fda526a9dca3106a6
fadi <fadel@uwo.ca>
2016-09-17 14:10:34 -0400

    first

---
+++ server/index.js
@@ -0,0 +1,56 @@
+'use strict';
+const electron = require('electron');
+
+const app = electron.app;
+
+// adds debug features like hotkeys for triggering dev tools and reload
+require('electron-debug')();
+
+// prevent window being garbage collected
+let mainWindow;
+
+function onClosed() {
+	// dereference the window
+	// for multiple windows store them in an array
+	mainWindow = null;
+}
+
+
+
+// TODO FIXED SIZE - DONE
+// TODO LOADER
+// TODO UPLOAD // DROP FOLDER
+
+// TODO RETRIEVE how many vault's there are
+// TODO Display all vaults
+
+
+function createMainWindow() {
+	const win = new electron.BrowserWindow({
+		width: 1200,
+		height: 800,
+		transparent: true,
+		frame: false
+	});
+
+	win.loadURL(`file://${__dirname}/index.html`);
+	win.on('closed', onClosed);
+
+	return win;
+}
+
+app.on('window-all-closed', () => {
+	if (process.platform !== 'darwin') {
+		app.quit();
+	}
+});
+
+app.on('activate', () => {
+	if (!mainWindow) {
+		mainWindow = createMainWindow();
+	}
+});
+
+app.on('ready', () => {
+	mainWindow = createMainWindow();
+});
