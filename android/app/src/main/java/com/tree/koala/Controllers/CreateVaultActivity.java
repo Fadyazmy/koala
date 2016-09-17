1d6fda2ba9f6cd64f858b5cad185ebcf21a8ec39
Elad Keyshawn <elad.keyshawn@gmail.com>
2016-09-17 10:01:24 -0400

    added basic creating page

---
+++ android/app/src/main/java/com/tree/koala/Controllers/CreateVaultActivity.java
@@ -0,0 +1,14 @@
+package com.tree.koala.Controllers;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import com.tree.koala.R;
+
+public class CreateVaultActivity extends AppCompatActivity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_create_vault);
+  }
+}
