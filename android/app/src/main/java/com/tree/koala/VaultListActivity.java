d39e779d1b9bb9cd0b82c4af0e56484afb2bce04
Jashan S <jashan1998s@gmail.com>
2016-09-17 01:36:04 -0400

    Created Android Project

---
+++ android/app/src/main/java/com/tree/koala/VaultListActivity.java
@@ -0,0 +1,13 @@
+package com.tree.koala;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class VaultListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_vault_list);
+    }
+}
