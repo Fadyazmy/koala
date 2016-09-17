ec835782df373dfa1348956600dd77a237295b42
Jashan S <jashan1998s@gmail.com>
2016-09-17 09:23:26 -0400

    Added RecyclerView and set up Maps

--- android/app/src/main/java/com/tree/koala/Controllers/VaultListActivity.java
+++ android/app/src/main/java/com/tree/koala/Controllers/VaultListActivity.java
@@ -3,6 +3,7 @@ package com.tree.koala.Controllers;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.mapbox.mapboxsdk.MapboxAccountManager;
 import com.tree.koala.R;
 
 public class VaultListActivity extends AppCompatActivity {
@@ -10,6 +11,8 @@ public class VaultListActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        // TODO insert MapBox access token
+        MapboxAccountManager.start(this, "");
         setContentView(R.layout.activity_vault_list);
     }
 }
--- android/app/src/main/java/com/tree/koala/Models/Vault.java
+++ android/app/src/main/java/com/tree/koala/Models/Vault.java
@@ -1,14 +1,26 @@
 package com.tree.koala.Models;
 
 import android.location.Location;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.TextView;
+
+import com.mapbox.mapboxsdk.annotations.MarkerOptions;
+import com.mapbox.mapboxsdk.geometry.LatLng;
+import com.mapbox.mapboxsdk.maps.MapView;
+import com.mapbox.mapboxsdk.maps.MapboxMap;
+import com.mapbox.mapboxsdk.maps.OnMapReadyCallback;
+import com.mikepenz.fastadapter.items.AbstractItem;
+import com.tree.koala.R;
 
 import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by Jashan Shewakramani
  * Description: Class for handling and storing vault data
  */
-public class Vault {
+public class Vault extends AbstractItem<Vault, Vault.ViewHolder>{
     private String mVaultName; // the vault name
     private Location mLocation;
     private ArrayList<VaultFile> mVaultFiles;
@@ -52,5 +64,47 @@ public class Vault {
 
     public void setVaultId(String vaultId) {
         mVaultId = vaultId;
+    }
+
+    @Override
+    public int getType() {
+        return 0;
+    }
+
+    @Override
+    public int getLayoutRes() {
+        return R.layout.vault_item;
+    }
+
+    @Override
+    public void bindView(final ViewHolder holder, List payloads) {
+        super.bindView(holder, payloads);
+
+        holder.vaultTitleText.setText(mVaultName);
+        holder.vaultDescriptionText.setText(String.format("%d files", mVaultFiles.size()));
+
+        holder.mapView.getMapAsync(new OnMapReadyCallback() {
+            @Override
+            public void onMapReady(MapboxMap mapboxMap) {
+                // display the location of the vault on the map using a marker
+                mapboxMap.addMarker(new MarkerOptions()
+                        .position(new LatLng(mLocation.getLatitude(), mLocation.getLongitude()))
+                        .title(mVaultName));
+            }
+        });
+    }
+
+    protected static class ViewHolder extends RecyclerView.ViewHolder {
+        protected MapView mapView;
+        protected TextView vaultTitleText;
+        protected TextView vaultDescriptionText;
+
+
+        public ViewHolder(View view) {
+            super(view);
+            mapView = (MapView) view.findViewById(R.id.vault_map);
+            vaultTitleText = (TextView) view.findViewById(R.id.vault_title);
+            vaultDescriptionText = (TextView) view.findViewById(R.id.vault_description);
+        }
     }
 }
