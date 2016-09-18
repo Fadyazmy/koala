3cd0f52f9e0925f38344b47842edff5383db39d2
Elad Keyshawn <elad.keyshawn@gmail.com>
2016-09-17 20:54:36 -0400

    added route to vault inside vault item card

--- android/app/src/main/java/com/tree/koala/Controllers/VaultListActivity.java
+++ android/app/src/main/java/com/tree/koala/Controllers/VaultListActivity.java
@@ -1,6 +1,7 @@
 package com.tree.koala.Controllers;
 
 import android.content.Intent;
+import android.location.Location;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -9,23 +10,28 @@ import android.view.View;
 
 import com.mapbox.mapboxsdk.MapboxAccountManager;
 import com.mikepenz.fastadapter.adapters.FastItemAdapter;
-import com.tree.koala.Controllers.CreateVaultActivity;
+import com.tree.koala.Models.Vault;
+import com.tree.koala.Models.VaultAdapter;
 import com.tree.koala.R;
+import com.tree.koala.utils.Constants;
 
 public class VaultListActivity extends AppCompatActivity {
 
     RecyclerView mRecyclerView;
     FloatingActionButton mCreateVaultButton;
+    private FastItemAdapter<VaultAdapter> adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        MapboxAccountManager.start(this, "pk.eyJ1IjoiamFzaGFuczk4IiwiYSI6ImNpdDc4YXJ1YTA3YW8yenAxZDRiM3I1dWIifQ.9cdUHw7KF4w06f2c0VFs7w");
+        MapboxAccountManager.start(this, Constants.MapboxToken);
         setContentView(R.layout.activity_vault_list);
 
         mRecyclerView = (RecyclerView) findViewById(R.id.vault_recycler_view);
-        FastItemAdapter fastItemAdapter = new FastItemAdapter();
-        mRecyclerView.setAdapter(fastItemAdapter);
+        adapter = new FastItemAdapter<VaultAdapter>();
+        mRecyclerView.setAdapter(adapter);
+
+
 
         mCreateVaultButton = (FloatingActionButton) findViewById(R.id.create_vault_button);
         mCreateVaultButton.setOnClickListener(new View.OnClickListener() {
@@ -37,5 +43,16 @@ public class VaultListActivity extends AppCompatActivity {
             }
         });
 
+
+        testAdapter();
+    }
+
+    private void testAdapter() {
+
+        Location currLocation = new Location("");//provider name is unecessary
+        currLocation.setLatitude(-118.24233);//your coords of course
+        currLocation.setLongitude( 34.05332);
+
+        adapter.add(new VaultAdapter(new Vault("Home", currLocation, null, "123"),this));
     }
 }
